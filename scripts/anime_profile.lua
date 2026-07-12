71734f7e2632227772b042b4865cb084ec6ab8f9
ilovealienz <github@brightlittlestars.moe>
2026-07-13 00:23:21 +0100

    anime_profile: add enabled/shaders toggle options

--- scripts/anime_profile.lua
+++ scripts/anime_profile.lua
@@ -1,15 +1,28 @@
 -- anime_profile.lua
 
+local options = require "mp.options"
+
+local o = {
+    enabled = true,   -- disable the whole script
+    shaders = true,   -- disable just the Anime4K shader application
+}
+options.read_options(o, "anime_profile")
+
 local anime_profile_applied = false
 
 local function apply_anime_profile()
+    if not o.enabled then
+        mp.osd_message("Anime profile: disabled")
+        return
+    end
+
     -- Reset subs
     mp.set_property("sub-scale", "1.0")
     mp.set_property("sub-pos", "100")
 
     -- If over 480p, apply Anime4K HQ via shaders.lua
     local h = mp.get_property_number("video-params/h")
-    if h and h > 480 then
+    if o.shaders and h and h > 480 then
         mp.commandv("script-message", "apply-shader-family", "CTRL+1", "Anime profile loaded")
     else
         mp.osd_message("Anime profile: subs reset")
@@ -23,6 +36,7 @@ end
 
 -- Auto-apply once on first file load
 mp.register_event("file-loaded", function()
+    if not o.enabled then return end
     local path = mp.get_property("path")
     if not anime_profile_applied and is_anime_path(path) then
         apply_anime_profile()
